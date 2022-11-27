2fa0f48dc2ab2825fa50d2f3fc27f6a28b5f6fb7
Miguel Angel Gonzales <miguelangel.gonzales@outlook.com>
2022-11-27 19:00:09 -0400

    clase 48: definiendo estado inicial del widget

--- base-weather-app/js/draggable.js
+++ base-weather-app/js/draggable.js
@@ -1,5 +1,5 @@
 const defaultConfig = {
-  open: true,
+  open: false,
   debug: true,
   animatable: true,
 };
@@ -11,9 +11,38 @@ export default function dragabble($element, config = defaultConfig) {
     );
   }
 
+  let isOpen = true;
+  const elementRect = $element.getBoundingClientRect();
+  const ELEMENT_BLOCK_SIZE = elementRect.height;
+  const $marker = $element.querySelector("[data-marker]");
+  const MARKER_BLOCK_SIZE = $marker.getBoundingClientRect().height;
+
+  const VISIBLE_Y_POSITION = 0;
+  const HIDDEN_Y_POSITION = ELEMENT_BLOCK_SIZE - MARKER_BLOCK_SIZE;
+  let widgetPosition = VISIBLE_Y_POSITION;
+  isOpen ? open() : close();
+
   function logger(message) {
     if (config.debug) {
       console.info(message);
     }
+  }
+
+  function open() {
+    logger("Abrir Widget");
+    isOpen = true;
+    widgetPosition = VISIBLE_Y_POSITION;
+    setWidgetPosition(widgetPosition);
+  }
+
+  function close() {
+    logger("Abrir Widget");
+    isOpen = false;
+    widgetPosition = HIDDEN_Y_POSITION;
+    setWidgetPosition(widgetPosition);
+  }
+
+  function setWidgetPosition(value) {
+    $element.style.marginBottom = `-${value}px`;
   }
 }
