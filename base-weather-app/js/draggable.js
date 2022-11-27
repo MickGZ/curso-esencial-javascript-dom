c0c9333e0eaf92f2d54d0e7ce1c6b66f301b9985
Miguel Angel Gonzales <miguelangel.gonzales@outlook.com>
2022-11-27 19:07:02 -0400

    clase 49: definiendo eventos de interaccion

--- base-weather-app/js/draggable.js
+++ base-weather-app/js/draggable.js
@@ -1,5 +1,5 @@
 const defaultConfig = {
-  open: false,
+  open: true,
   debug: true,
   animatable: true,
 };
@@ -12,6 +12,7 @@ export default function dragabble($element, config = defaultConfig) {
   }
 
   let isOpen = true;
+  let isDragging = false;
   const elementRect = $element.getBoundingClientRect();
   const ELEMENT_BLOCK_SIZE = elementRect.height;
   const $marker = $element.querySelector("[data-marker]");
@@ -21,6 +22,47 @@ export default function dragabble($element, config = defaultConfig) {
   const HIDDEN_Y_POSITION = ELEMENT_BLOCK_SIZE - MARKER_BLOCK_SIZE;
   let widgetPosition = VISIBLE_Y_POSITION;
   isOpen ? open() : close();
+
+  $marker.addEventListener("click", handleClick);
+  $marker.addEventListener("pointerdown", handlePointerDown);
+  $marker.addEventListener("pointerup", handlePointerUp);
+  $marker.addEventListener("pointerout", handlePointerOut);
+  $marker.addEventListener("pointercancel", handlePointerCancel);
+  $marker.addEventListener("pointermove", handlePointerMove);
+
+  function handlePointerUp() {
+    logger("Pointer UP");
+  }
+
+  function handlePointerOut() {
+    logger("Pointer OUT");
+  }
+
+  function handlePointerCancel() {
+    logger("Pointer Cancel");
+  }
+
+  function handlePointerDown() {
+    logger("Pointer Down");
+  }
+
+  function handleClick(event) {
+    logger("CLICK");
+    toggle();
+  }
+
+  function handlePointerMove() {
+    logger("Pointer MOVE");
+  }
+
+  function toggle() {
+    if (!isDragging) {
+      if (!isOpen) {
+        return open();
+      }
+      return close();
+    }
+  }
 
   function logger(message) {
     if (config.debug) {
