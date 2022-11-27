b3bf996c975d405661c439dd4beaa31372940795
MGonzalesNova <MGonzales@mynovahealthcare.org>
2022-11-26 22:02:05 -0400

    clase 46: renderizando pronostico del clima por dia

--- base-weather-app/js/weekly-weather.js
+++ base-weather-app/js/weekly-weather.js
@@ -1,9 +1,42 @@
 import { getWeeklyWeather } from "./services/weather.js";
 import { formatWeekList } from "./utils/format-data.js";
 import { getLatLon } from "./geolocation.js";
+import { createDOM } from "./utils/dom.js";
+import { createPeriodTime } from "./period-time.js";
 
-function configWeeklyWeather() {}
+function tabPanelTemplate(id) {
+  return `
+    <div class="tabPanel" tabindex="0" aria-labelledby="tab-${id}">
+        <div class="dayWeather" id="dayWeather-${id}">
+        <ul class="dayWeather-list" id="dayWeather-list-${id}">
+        </ul>
+        </div>
+    </div>
+  `;
+}
 
+function createTabPanel(id) {
+  const $panel = createDOM(tabPanelTemplate(id));
+  if (id > 0) {
+    $panel.hidden = true;
+  }
+  return $panel;
+}
+
+function configWeeklyWeather(weekList) {
+  const $container = document.querySelector(".tabs");
+  weekList.forEach((day, index) => {
+    const $panel = createTabPanel(index);
+    $container.append($panel);
+    day.forEach((weather, indexWeather) => {
+      $panel
+        .querySelector(".dayWeather-list")
+        .append(createPeriodTime(weather));
+    });
+  });
+}
+
+// The forecast prediction
 export default async function weeklyWeather() {
   const { lat, lon, isError } = await getLatLon();
   if (isError) return console.log("A ocurrido un error ubicandote");
